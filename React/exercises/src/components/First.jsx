1beca7b0090b67cf839167f482703e68c39bb67f
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-25 13:58:36 -0300

    Multiples components

--- React/exercises/src/components/First.jsx
+++ React/exercises/src/components/First.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 import GoodMorning from "./GoodMorning";
+import GoodAfternoon, { /* GoodAfternoon */ GoodNight } from './Multiples'
 
 // or 
 // export default () => <h1>First</h1> 
 export default function(){
-  return <GoodMorning name='Isaac' age={18}/>
+  return (
+
+    <div>
+      <GoodMorning name='Isaac' age={18}/>
+      <GoodAfternoon name='Fulano' />
+      <GoodNight name='Beltrano' />
+    </div>
+    
+  );
 }
---
+++ React/exercises/src/components/Multiples.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const GoodAfternoon = props => <h4>Good Afternoon <b>{props.name}</b>.</h4>
+
+export const GoodNight = props => <h4>Good Night <b>{props.name}</b>.</h4>
+
+export default GoodAfternoon;
