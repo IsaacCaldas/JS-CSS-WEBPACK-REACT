4b7e86c0024ebb80630fba0571f6a2bfb9cfae8d
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-26 09:35:34 -0300

    Project CRUD finished

--- React/CRUD/frontend/src/components/user/UserCrud.jsx
+++ React/CRUD/frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import axios from 'axios';
+
+import './UserCrud.css';
+
 import Main from "../template/Main";
 
 const headerProps = {
@@ -7,16 +11,163 @@ const headerProps = {
   subtitle: 'User CRUD - Create / Read / Update / Delete'
 }
 
+const baseURL = 'http://localhost:3003/users';
+
+const initialState = {
+  user: {
+    name: '',
+    email: '',
+  },
+  list: []
+}
+
 class UserCrud extends Component {
+
+  state = {...initialState};
+
+  componentWillMount(){
+    axios(baseURL).then(resp => {
+      this.setState({list: resp.data})
+    });
+  }
+
+  clear(){
+    this.setState({
+      user: initialState.user
+    })
+  }
+
+  save(){
+    const user = this.state.user;
+    const method = user.id ? 'put' : 'post';
+    const URL = user.id ? `${baseURL}/${user.id}` : baseURL;
+
+    axios[method](URL, user).then(resp => {
+      const list = this.getUpdatedList(resp.data);
+      this.setState({
+        user: initialState.user, list
+      });
+    });
+  }
+
+  getUpdatedList(user, add = true){
+    const list = this.state.list.filter(u => u.id !== user.id);
+    if(add) list.unshift(user);
+
+    return list;
+  }
+
+  updateField(event){
+    const user = {...this.state.user}
+    user[event.target.name] = event.target.value;
+
+    this.setState({ user });
+  }
+
+  renderForm(){
+    return (
+
+      <div className="form">
+        <div className="row">
+
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Name</label>
+              <input type="text" className="form-control"
+                name="name" 
+                value={this.state.user.name} 
+                onChange={e => this.updateField(e)} 
+                placeholder="Write the name"
+              />
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>E-mail</label>
+              <input type="text" className="form-control"
+                name="email" 
+                value={this.state.user.email} 
+                onChange={e => this.updateField(e)} 
+                placeholder="Write the e-mail"
+              />
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-12 d-flex justify-content-end">
+
+              <button className="btn btn-primary" onClick={e => this.save(e)}>Submit</button>
+              <button className="btn btn-secondary ml-2" onClick={e => this.clear(e)}>Cancel</button>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    )
+  }
+
+  load(user){
+    this.setState({user})
+  }
+  remove(user){
+    axios.delete(`${baseURL}/${user.id}`).then(resp => {
+      const list = this.getUpdatedList(user, false);
+      this.setState({list})
+    })
+  }
+
+  renderTable(){
+
+    return (
+
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>E-mail</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderRows()}
+        </tbody>
+      </table>
+
+    );
+
+  }
+  renderRows(){
+    return this.state.list.map(user => {
+      return (
+        <tr className="rows" key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>
+            <button className="btn btn-warning" onClick={() => this.load(user)}>
+              <i className="fa fa-pencil"></i>
+            </button>
+            <button className="btn btn-danger ml-2" onClick={() => this.remove(user)}>
+              <i className="fa fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      )
+    });
+  }
 
   render(){
 
     return (
 
       <Main {...headerProps}>
-        User register
+        {this.renderForm()}
+        {this.renderTable()}
       </Main>
-
     );
   }
 }
